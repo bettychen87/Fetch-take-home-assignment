9b85c7e7cfc183e87a233783beeecaa4e5ee840e
Betty Chen <betty87@uab.edu>
2023-10-14 15:04:52 -0500

    added point calculations

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import datetime
 from flask import Flask, request, jsonify
+import math
 import uuid
 
 app = Flask(__name__)
@@ -8,6 +10,31 @@ receipts = {}
 
 def calculate_points(receipt_data):
     points = 0
+    for char in receipt_data["retailer"]:
+        if char.isalnum():
+            points += 1
+    if receipt_data["total"] % 1 == 0:
+        points += 50
+    if receipt_data["total"] % 0.25 == 0:
+        points += 25
+    
+    item_points = (len(receipt_data["items"]) // 2) * 5
+    points += item_points
+
+    for item in receipt_data["item"]:
+        if len(item["description"].strip()) % 3 == 0:
+            points += math.ceil(item["price"] * 0.2)
+
+        day = int(item["purchaseDate"].split("-")[2])
+        if day % 2 == 1:
+            points += 6
+
+        purchase_time = datetime.strptime(item["purchaseTime"], "%H:%M:%S").time()
+        lower_bound = datetime.time(14, 0, 0)
+        upper_bound = datetime.time(16, 0, 0)
+
+        if lower_bound < purchase_time < upper_bound:
+            points += 10
 
     return points
 
