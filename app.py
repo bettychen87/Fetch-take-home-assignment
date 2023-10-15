92aa6d19b1138f1ccc9429bb040a20d3d9bff308
Betty Chen <betty87@uab.edu>
2023-10-15 18:24:35 -0500

    fixing errors

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ def calculate_points(receipt_data):
         points += 50
     if receipt_data["total"] % 0.25 == 0:
         points += 25
-    
+
     item_points = (len(receipt_data["items"]) // 2) * 5
     points += item_points
 
-    for item in receipt_data["item"]:
+    for item in receipt_data["items"]:  # Use "items" instead of "item"
         if len(item["description"].strip()) % 3 == 0:
             points += math.ceil(item["price"] * 0.2)
 
@@ -29,7 +29,7 @@ def calculate_points(receipt_data):
         if day % 2 == 1:
             points += 6
 
-        purchase_time = datetime.strptime(item["purchaseTime"], "%H:%M:%S").time()
+        purchase_time = datetime.datetime.strptime(item["purchaseTime"], "%H:%M:%S").time()
         lower_bound = datetime.time(14, 0, 0)
         upper_bound = datetime.time(16, 0, 0)
 
@@ -38,26 +38,26 @@ def calculate_points(receipt_data):
 
     return points
 
-@app.route('/receipt/process', methods = ['POST'])
+@app.route('/receipt/process', methods=['POST'])
 def process_receipts():
     receipt_data = request.get_json()
     receipt_id = str(uuid.uuid4())
 
     receipt_points = calculate_points(receipt_data)
-    receipts[receipt_data] = {
+    receipts[receipt_id] = {
         "data": receipt_data,
         "points": receipt_points
     }
 
     return jsonify({"receipt_id": receipt_id})
 
-@app.route('/receipts/<uuid:receipt_id>/points', methods = ['GET'])
+@app.route('/receipts/<uuid:receipt_id>/points', methods=['GET'])
 def get_receipt_points(receipt_id):
     if receipt_id not in receipts:
-        return jsonify({"error": "Receipt not found"})
+        return jsonify({"error": "Receipt not found"}), 404
     else:
         receipt_points = receipts[receipt_id]["points"]
-        return jsonify({"points": receipt_points}), 404
+        return jsonify({"points": receipt_points})
 
 if __name__ == "__main__":
-   app.run(debug = True)
+   app.run(debug=True)
