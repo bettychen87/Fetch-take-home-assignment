beaa59e7e38c899f2d4ef55e826c720ce904d19e
Betty Chen <betty87@uab.edu>
2023-10-18 13:36:08 -0500

    added gitignore

--- app.py
+++ app.py
@@ -13,46 +13,52 @@ def calculate_points(receipt_data):
     for char in receipt_data["retailer"]:
         if char.isalnum():
             points += 1
-    if receipt_data["total"] % 1 == 0:
+    
+    total = float(receipt_data["total"])
+    if total % 1 == 0:
         points += 50
-    if receipt_data["total"] % 0.25 == 0:
+    if total % 0.25 == 0:
         points += 25
 
     item_points = (len(receipt_data["items"]) // 2) * 5
     points += item_points
 
-    for item in receipt_data["items"]:  # Use "items" instead of "item"
-        if len(item["description"].strip()) % 3 == 0:
-            points += math.ceil(item["price"] * 0.2)
+    for item in receipt_data["items"]:
+        if len(item["shortDescription"].strip()) % 3 == 0:
+            price = float(item["price"])
+            points += math.ceil(price * 0.2)
 
-        day = int(item["purchaseDate"].split("-")[2])
-        if day % 2 == 1:
-            points += 6
+    day = int(receipt_data["purchaseDate"].split("-")[2])
+    if day % 2 == 1:
+        points += 6
 
-        purchase_time = datetime.datetime.strptime(item["purchaseTime"], "%H:%M:%S").time()
-        lower_bound = datetime.time(14, 0, 0)
-        upper_bound = datetime.time(16, 0, 0)
+    purchase_time = datetime.datetime.strptime(receipt_data["purchaseTime"], "%H:%M").time()
+    lower_bound = datetime.time(14, 0, 0)
+    upper_bound = datetime.time(16, 0, 0)
 
-        if lower_bound < purchase_time < upper_bound:
-            points += 10
+    if lower_bound < purchase_time < upper_bound:
+        points += 10
 
     return points
 
-@app.route('/receipt/process', methods=['POST'])
+@app.route('/receipts/process', methods=['POST'])
 def process_receipts():
     receipt_data = request.get_json()
     receipt_id = str(uuid.uuid4())
 
+    print(receipt_data)
     receipt_points = calculate_points(receipt_data)
     receipts[receipt_id] = {
         "data": receipt_data,
         "points": receipt_points
     }
-
     return jsonify({"receipt_id": receipt_id})
 
 @app.route('/receipts/<uuid:receipt_id>/points', methods=['GET'])
 def get_receipt_points(receipt_id):
+
+    print (receipts)
+
     if receipt_id not in receipts:
         return jsonify({"error": "Receipt not found"}), 404
     else:
